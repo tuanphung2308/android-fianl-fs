26fb5c01019f0dcc45fa5815cac4c534cfab81a5
tuan <tuanphung2308@gmail.com>
2021-01-27 12:14:19 +0700

    fuck this shit firebase

--- app/src/main/java/com/google/firebase/example/fireeats/java/tuan/ShoppingCartSimple.java
+++ app/src/main/java/com/google/firebase/example/fireeats/java/tuan/ShoppingCartSimple.java
@@ -3,24 +3,78 @@ package com.google.firebase.example.fireeats.java.tuan;
 import android.content.Intent;
 import android.graphics.PorterDuff;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.example.fireeats.R;
+import com.google.firebase.example.fireeats.java.adapter.CartObjectAdapter;
+import com.google.firebase.example.fireeats.java.model.Cart;
 import com.google.firebase.example.fireeats.java.utils.Tools;
+import com.google.firebase.example.fireeats.java.viewmodel.MainActivityViewModel;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 
 public class ShoppingCartSimple extends AppCompatActivity {
+    private DocumentReference cartRef;
+    private CartObjectAdapter cartObjectAdapter;
+    private RecyclerView recyclerView;
+    private FirebaseFirestore mFirestore;
+    private static final String TAG = "ShoppingCartSimple";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shopping_cart_simple);
         initToolbar();
+
+        // Enable Firestore logging
+        FirebaseFirestore.setLoggingEnabled(true);
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        // Firestore
+        mFirestore = FirebaseFirestore.getInstance();
+
+        cartRef = mFirestore.collection("carts").document(user.getUid());
+        recyclerView = findViewById(R.id.recyclerCartObjects);
+
+        cartRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        Log.d(TAG, "Current data: " + document.getData());
+                        Cart cart = document.toObject(Cart.class);
+                        Log.d(TAG, "Current data: " + cart.getCartObjectList().size());
+
+                        cartObjectAdapter = new CartObjectAdapter(cart.getCartObjectList());
+                        recyclerView.setAdapter(cartObjectAdapter);
+                        cartObjectAdapter.notifyDataSetChanged();
+                        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
+                        recyclerView.setLayoutManager(mLayoutManager);
+                        recyclerView.setItemAnimator(new DefaultItemAnimator());
+                    } else {
+                        Log.d(TAG, "No such document");
+                    }
+                } else {
+                    Log.d(TAG, "get failed with ", task.getException());
+                }
+            }
+        });
     }
     private void initToolbar() {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
