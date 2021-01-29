666c7c1290e6d1587bec091b7e9e925cf84b157a
tuan <tuanphung2308@gmail.com>
2021-01-29 12:04:12 +0700

    wang isnoob

--- app/src/main/java/com/google/firebase/example/fireeats/java/activities/ListProductViewActivity.java
+++ app/src/main/java/com/google/firebase/example/fireeats/java/activities/ListProductViewActivity.java
@@ -1,5 +1,7 @@
 package com.google.firebase.example.fireeats.java.activities;
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,7 +11,9 @@ import android.view.View;
 import android.widget.LinearLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.MenuItemCompat;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -23,8 +27,10 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.Query;
 
+import java.util.Arrays;
+
 public class ListProductViewActivity extends AppCompatActivity
-        implements RestaurantAdapter.OnRestaurantSelectedListener {
+        implements RestaurantAdapter.OnRestaurantSelectedListener, SearchView.OnQueryTextListener {
     public static final String KEY_RESTAURANT_ID = "key_restaurant_id";
     private static final String KEY_CATEGORY_ID = "CATEGORY";
     private RecyclerView recyclerView;
@@ -32,17 +38,21 @@ public class ListProductViewActivity extends AppCompatActivity
     private FirebaseFirestore mFirestore;
     private Query mQuery;
     private RestaurantAdapter mAdapter;
+    private String category;
+    private RecyclerView recyclerRestaurants;
+    private LinearLayout viewEmpty;
+    private SearchView searchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shopping_product_grid);
 
-        RecyclerView recyclerRestaurants = findViewById(R.id.recyclerRestaurants);
-        LinearLayout viewEmpty = findViewById(R.id.viewEmpty);
+        recyclerRestaurants = findViewById(R.id.recyclerRestaurants);
+        viewEmpty = findViewById(R.id.viewEmpty);
 
         // Get restaurant ID from extras
-        String category = getIntent().getExtras().getString(KEY_CATEGORY_ID);
+        category = getIntent().getExtras().getString(KEY_CATEGORY_ID);
 
         // Enable Firestore logging
         FirebaseFirestore.setLoggingEnabled(true);
@@ -50,7 +60,8 @@ public class ListProductViewActivity extends AppCompatActivity
         mFirestore = FirebaseFirestore.getInstance();
 
         // Get ${LIMIT} restaurants
-        mQuery = mFirestore.collection("products").whereEqualTo(Product.FIELD_CATEGORY, category);;
+        mQuery = mFirestore.collection("products").whereEqualTo(Product.FIELD_CATEGORY, category);
+        ;
 
         // RecyclerView
         mAdapter = new RestaurantAdapter(mQuery, this) {
@@ -92,7 +103,17 @@ public class ListProductViewActivity extends AppCompatActivity
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_cart_setting, menu);
+        getMenuInflater().inflate(R.menu.menu_cart_setting2, menu);
+        SearchManager searchManager =
+                (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        searchView =
+                (SearchView) menu.findItem(R.id.action_search).getActionView();
+        searchView.setSearchableInfo(
+                searchManager.getSearchableInfo(getComponentName()));
+        searchView.setIconified(true);
+        searchView.setVisibility(View.GONE);
+        searchView.setQueryHint("Search");
+        searchView.setOnQueryTextListener(this);
         return true;
     }
 
@@ -100,10 +121,11 @@ public class ListProductViewActivity extends AppCompatActivity
     public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == android.R.id.home) {
             finish();
-        } else {
+        } else if (item.getItemId() == R.id.action_cart) {
             Intent intent = new Intent(this, ShoppingCartActivity.class);
             intent.putExtra("fuck you", "fuck");
             startActivity(intent);
+        } else if (item.getItemId() == R.id.action_search) {
         }
         return super.onOptionsItemSelected(item);
     }
@@ -134,5 +156,42 @@ public class ListProductViewActivity extends AppCompatActivity
         intent.putExtra(KEY_RESTAURANT_ID, restaurant.getId());
         startActivity(intent);
         overridePendingTransition(R.anim.slide_in_from_right, R.anim.slide_out_to_left);
+    }
+
+    public void setNewQuery() {
+        mAdapter.stopListening();
+        // RecyclerView
+        mAdapter = new RestaurantAdapter(mQuery, this) {
+            @Override
+            protected void onDataChanged() {
+                // Show/hide content if the query returns empty.
+            }
+
+            @Override
+            protected void onError(FirebaseFirestoreException e) {
+                // Show a snackbar on errors
+                Log.d("Error", "check logs for info.");
+            }
+        };
+
+        recyclerRestaurants.setLayoutManager(new GridLayoutManager(this, 2));
+        recyclerRestaurants.setAdapter(mAdapter);
+        mAdapter.startListening();
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String query) {
+//                searchAdapter.getFilter().filter(query);
+        mQuery = mFirestore.collection("products").whereEqualTo(Product.FIELD_CATEGORY, category)
+                .orderBy("name")
+                .startAt(query)
+                .endAt(query+"\uf8ff");
+        setNewQuery();
+        return true;
     }
 }
